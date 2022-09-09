95d90f2c88231bacd5d867e94e95d05bd571aef3
Justin Sohn <sohnjustin2@gmail.com>
2022-09-09 04:58:30 -0700

    small tweaks

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,21 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route} from 'react-router-dom';
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Experiences from './styles/routelayout/Experience';
 import Home from './styles/routelayout/Home';
 import Projects from './styles/routelayout/Projects';
 import NavBar from "./components/NavBar";
-
+import "./styles/NavBar.css";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
+    <div className= "App">
       <BrowserRouter>
-        <Routes>
-          <Route path ="/" element = {<Home />} />
-          <Route path ="/projects" element = {<Projects />} />
-          <Route path ="/experience" element = {<Experiences />} />
-        </Routes>
+        <NavBar/>
+          <Routes>
+            <Route path ="/" element ={<Home />} />
+            <Route path ="/projects" element ={<Projects />} />
+            <Route path ="/experience" element ={<Experiences />} />
+          </Routes>
+          <Footer/>
       </BrowserRouter>
-    </div>
+      </div>
   );
 }
 
