10387e787912a2ac07f266997571b460c9ff215f
Justin Sohn <sohnjustin2@gmail.com>
2022-09-16 22:10:11 -0700

    staging missing files

---
+++ portfolio/src/styles/pageTransition.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+const pageTransition = {
+    in:{
+        opacity: 1,
+        x:0
+    },
+    out:{
+        opacity: 0,
+        x: "-100vw"
+    }
+
+}
+export default pageTransition;
